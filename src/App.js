edfffe2733908c2b1feb2af40249fe044e2b574c
Saloni180430 <156230375+Saloni180430@users.noreply.github.com>
2024-08-03 16:32:50 +0530

    completed project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordian from './components/accordian';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      <Accordian/>
+      <Accordian />
     </div>
   );
 }
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+import data from './data';
+import './style.css';
+
 export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+
+    const handleSingleSelect = (id) => {
+        setSelected(selected === id ? null : id)
+    }
+
+    const handleMultipleSelect = (id) => {
+        let newMultiple = [...multiple]
+
+        if (newMultiple.includes(id)) {
+            newMultiple = newMultiple.filter((i) => i !== id)
+        } else {
+            newMultiple.push(id)
+        }
+        setMultiple(newMultiple)
+    }
+
     return (
-        <div>
-            <h1>Accordian</h1>
+        <div className="wrapper">
+            <button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>Enable Multi Selection</button>
+            <div classname="accordian">
+                {
+                    data && data.length > 0 ?
+                        data.map((item) => (
+                            <div className='item'>
+                                <div className='title'>
+                                    <h3>{item.question}</h3>
+                                    <span className='icon' onClick={
+                                        enableMultiSelect
+                                            ? () => handleMultipleSelect(item.id)
+                                            : () => handleSingleSelect(item.id)}>+</span>
+                                </div>
+                                {enableMultiSelect
+                                    ? multiple.includes(item.id)
+                                        ? <div className='content'>{item.answer}</div>
+                                        : null
+                                    : selected === item.id
+                                        ? <div className='content'>{item.answer}</div>
+                                        : null
+                                }
+                            </div>
+                        ))
+                        : <div>No data found</div>
+                }
+            </div>
+
         </div>
     );
 }
