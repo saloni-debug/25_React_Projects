96eda4410ad0d01ad4e6db89778c3899ee275b65
Saloni180430 <156230375+Saloni180430@users.noreply.github.com>
2024-08-06 16:47:36 +0530

    star rating project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 // import Accordian from './components/accordian';
-import RandomColorGenerator from './components/randomColorGenerator';
+// import RandomColorGenerator from './components/randomColorGenerator';
+import StarRating from './components/starRating';
 
 function App() {
   return (
     <div className="App">
+
       {/* Accordian Component */}
       {/* <Accordian /> */}
-      <RandomColorGenerator/>
+
+      {/* Random Color Generator Component */}
+      {/* <RandomColorGenerator/> */}
+
+      {/* Star Rating Component */}
+      <StarRating />
     </div>
   );
 }
---
+++ src/components/starRating/index.js
@@ -0,0 +1,26 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useState } from 'react';
+import './style.css';
+export default function StarRating() {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    const starArr = [1, 2, 3, 4, 5]
+    return (
+        <div className="wrapper">
+            <h1>Star Rating</h1>
+            <div className='rating'>
+                {starArr.map((star, index) => {
+                    const ratingValue = index + 1;
+                    return (
+                        <span onClick={() => setRating(ratingValue)}>
+                            <i className="far fa-star" style={{ color: (ratingValue <= (hover||rating)) ? "#ffc107" : "#e4e5e9" }}
+                                onMouseEnter={() => setHover(ratingValue)}
+                                onMouseLeave={() => setHover(0)}></i>
+                        </span>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
